da4b7e27eee9aaec0df73ad5020cec438ab5575e
Antonio7347 <20233tn106@utez.edu.mx>
2025-05-20 21:52:39 -0600

    Dios ayudame con esto xd

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import { AppLayout } from "./layout/AppLayout"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AppLayout } from "./layout/AppLayout";
+import { LandingPage } from './pages/LandingPage';
 
 function App() {
-
   return (
-    <>
-      <AppLayout
-        role="Admin"
-        name="Nombre del usuario"
-        email="Correo electrónico del usuario"
-      >
-        <div className='w-full h-full'>
-          <p>Contenido</p>
-        </div>
-      </AppLayout>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard/*" element={
+          <AppLayout
+            role="Admin"
+            name="Nombre del usuario"
+            email="Correo electrónico del usuario"
+          >
+            {/* Contenido del dashboard */}
+          </AppLayout>
+        }/>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
