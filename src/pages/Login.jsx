f523e4ecd73a14c3ccbf2da3a81307c465d1f1e9
AmadorCDR <amadorcasillasdr@gmail.com>
2025-08-13 19:01:31 -0600

    Cambio de localhost

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,7 +27,7 @@ export const Login = () => {
         try {
             setIsLoading(true);
 
-            const response = await fetch("http://localhost:8080/api/auth/login", {
+            const response = await fetch("http://192.168.1.76:8080/api/auth/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(data),
